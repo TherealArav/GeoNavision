316e71f0b7a269f7704b51cdc6fd1304c1c3101c
TherealArav <aravzgoyal@gmail.com>
2026-03-18 21:11:32 +0400

    UPDATE: App.py

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import streamlit as st
 intro_page = st.Page("pages/Introduction.py", title="Introduction")
 app_page = st.Page("pages/Main_App.py", title="Main Application")
 maps_page = st.Page("pages/Maps.py", title="Interactive Maps")
-image_analysis_page = st.Page("pages/Image_Analysis.py", title="Image Analysis")
 
 
 # Aranging the pages in a list for navigation
-pages = [intro_page, app_page, maps_page,image_analysis_page]
+pages = [intro_page, app_page, maps_page]
 pg = st.navigation(pages)
 pg.run()
