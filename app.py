043e968fd739925460b70fe7e3e7027a1a0c132c
TherealArav <aravzgoyal@gmail.com>
2026-04-20 12:21:52 +0400

    UPDATE: encap main application file within function mainApp() to indicate main script

--- app.py
+++ app.py
@@ -2,14 +2,22 @@
 
 import streamlit as st
 
+# Memory
+from memory_profiler import profile
 
-# Define the pages of the application
-intro_page = st.Page("pages/Introduction.py", title="Introduction")
-app_page = st.Page("pages/Main_App.py", title="Main Application")
-maps_page = st.Page("pages/Maps.py", title="Interactive Maps")
+@profile
+def mainApp():
+    # Define the pages of the application
+    intro_page = st.Page("pages/Introduction.py", title="Introduction")
+    app_page = st.Page("pages/Main_App.py", title="Main Application")
+    maps_page = st.Page("pages/Maps.py", title="Interactive Maps")
 
 
-# Aranging the pages in a list for navigation
-pages = [intro_page, app_page, maps_page]
-pg = st.navigation(pages)
-pg.run()
+    # Aranging the pages in a list for navigation
+    pages = [intro_page, app_page, maps_page]
+    pg = st.navigation(pages)
+    pg.run()
+
+
+if __name__ == "__main__":
+    mainApp()
